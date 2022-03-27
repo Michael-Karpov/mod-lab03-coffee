3a90717871614778a0ce81fdab3fc80e428da75e
Mihail Karpov <100800994+Michael-Karpov@users.noreply.github.com>
2022-03-28 00:00:17 +0300

    Update Automata.cpp

--- src/Automata.cpp
+++ src/Automata.cpp
@@ -4,78 +4,76 @@ using std::cout;
 using std::endl;
 
 Automata::Automata() {
-	Menu = new std::string[4];
-	Menu[0] = "Lemonade";
-	Menu[1] = "Hot Chocolate";
-	Menu[2] = "Coffee";
-	Menu[3] = "Water";
-	Prices = new int[4];
-	Prices[0] = 100;
-	Prices[1] = 200;
-	Prices[2] = 300;
-	Prices[3] = 400;
-	Cash = 0;
-	State = OFF;
+    Menu = new std::string[4];
+    Menu[0] = "Lemonade";
+    Menu[1] = "Hot Chocolate";
+    Menu[2] = "Coffee";
+    Menu[3] = "Water";
+    Prices = new int[4];
+    Prices[0] = 100;
+    Prices[1] = 200;
+    Prices[2] = 300;
+    Prices[3] = 400;
+    Cash = 0;
+    State = OFF;
 }
 void Automata::on() {
-	if (this->State == OFF) {
-		this->State = WAIT;
-	}
+    if (this->State == OFF) {
+	this->State = WAIT;
+    }
 }
 void Automata::off() {
-	if (this->State == WAIT) {
-		this->State = OFF;
-	}
+    if (this->State == WAIT) {
+	this->State = OFF;
+    }
 }
 void Automata::coin(int m) {
-	if (this->State == WAIT) {
-		this->State = ACCEPT;
-	}
-	Cash += m;
+    if (this->State == WAIT) {
+	this->State = ACCEPT;
+    }
+    Cash += m;
 }
 void Automata::getMenu() {
-	for (int i = 0; i < 4; i++) {
-		cout << i + 1 << ": " << Menu[i] << " " << Prices[i];
-		cout << '\n';
-	}
+    for (int i = 0; i < 4; i++) {
+	cout << i + 1 << ": " << Menu[i] << " " << Prices[i];
+	cout << '\n';
+    }
 }
 STATES Automata::getState() {
-	return State;
+    return State;
 }
 void Automata::choice(int order) {
-	if (this->State == ACCEPT) {
-		this->State = CHECK;
-		this->order = order;
-	}
+    if (this->State == ACCEPT) {
+	this->State = CHECK;
+	this->order = order;
+    }
 }
 bool Automata::check() {
-	if (this->State == CHECK && Cash >= Prices[order - 1]) return true;
-	return false;
+    if (this->State == CHECK && Cash >= Prices[order - 1]) 
+	    return true;
+	else return false;
 }
 int Automata::cancel() {
-	int cash2 = 0;
-	if (this->State == ACCEPT || this->State == CHECK) {
-		this->State = WAIT;
-		cash2 = Cash;
-		Cash = 0;
-	}
-	return cash2;
+    int cash2 = 0;
+    if (this->State == ACCEPT || this->State == CHECK) {
+	this->State = WAIT;
+	cash2 = Cash;
+	Cash = 0;
+    }
+    return cash2;
 }
 void Automata::cook() {
-	if (this->State == CHECK) {
-		this->State = COOK;
-		Cash -= Prices[order - 1];
-	}
+    if (this->State == CHECK) {
+        this->State = COOK;
+	Cash -= Prices[order - 1];
+    }
 }
 int Automata::finish() {
-	int sym = 0;
-	if (this->State == COOK) {
-		this->State = WAIT;
-		sym = Cash;
-		Cash = 0;
-	}
-	return sym;
+    int sym = 0;
+    if (this->State == COOK) {
+	this->State = WAIT;
+	sym = Cash;
+	Cash = 0;
+    }
+    return sym;
 }
-
-
-
