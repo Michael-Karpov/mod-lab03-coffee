c0e26d4f963b10d886cfa1d296607efab39114a2
Mihail Karpov <100800994+Michael-Karpov@users.noreply.github.com>
2022-03-27 22:36:43 +0300

    Create Automata.h

---
+++ include/Automata.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using std::string;
+
+enum STATES {
+	OFF, WAIT, ACCEPT, CHECK, COOK
+};
+
+class Automata {
+private:
+	int Cash;
+	string* Menu;
+	int* Prices;
+	STATES State;
+	int order;
+
+public:
+	Automata();
+	void on();
+	void off();
+	void coin(int);
+	void getMenu();
+	STATES getState();
+	void choice(int);
+	bool check();
+	int cancel();
+	void cook();
+	int finish();
+};
