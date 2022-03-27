a0910512cdf74a2f7d25d36d799f3c2ceca95070
Mihail Karpov <100800994+Michael-Karpov@users.noreply.github.com>
2022-03-27 22:46:04 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,8 @@ int main()
     Automata machine = Automata();
     machine.on();
     machine.getMenu();
-    machine.coin(150);
-    machine.choice(0);
+    machine.coin(300);
+    machine.choice(2);
     machine.finish();
     machine.coin(500);
     machine.coin(100);
