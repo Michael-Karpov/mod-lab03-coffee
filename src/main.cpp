958f3d2e0bcd9fc46fa3a2665518a03ea59ace50
Mihail Karpov <100800994+Michael-Karpov@users.noreply.github.com>
2022-03-28 00:57:29 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,17 @@
 #include "Automata.h"
 using namespace std;
 
-int main()
-{
+int main() {
+    Automata machine = Automata();
+    machine.on();
+    machine.getMenu();
+    machine.coin(400);
+    machine.choice(1);
+    machine.finish();
+    machine.coin(10);
+    machine.coin(150);
+    machine.choice(3);
+    machine.finish();
+    machine.off();
     return 0;
 }
